daed1cb03d6be4dd4a236e19296db77c475060cf
Bernardo <bernardomarques.gv@gmail.com>
2026-05-10 18:56:59 -0300

    Implement folder selection and update UI elements
    Added folder selection functionality and updated UI labels.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import customtkinter as ctk
 from pathlib import Path
+from tkinter import filedialog
 
 selected_template = None
+selected_folder = None
 
 structures = {
     'Python CLI': [
@@ -62,6 +64,11 @@ RepoForge.geometry('400x600')
 font1 = ctk.CTkFont(family='Times New Roman', size=12)
 font2 = ctk.CTkFont(family='Times New Roman', size=25)
 
+title = ctk.CTkLabel(RepoForge, text='RepoForge', font=font2)
+desc = ctk.CTkLabel(RepoForge, text='A new way to create repositories', font=font1)
+
+title.pack(pady=10)
+desc.pack()
 ctk.set_appearance_mode('dark')
 
 def create_repo():
@@ -69,16 +76,24 @@ def create_repo():
     name = name_project.get()
     description = desc_project.get()
     files = structures.get(selected_template)
-   
+    
     if Path(name).exists():
         info_label.configure(text=f'name {name} alredy exists!', text_color = 'red')
         return
     
     for item in files:
         
-        archive = Path(name) / item
+        if not selected_folder:
+            info_label.configure(text='Please, select a folder first!', text_color='red')
+            return
+
+        archive = Path(selected_folder) / name / item
+
         if item.endswith('/'):
             archive.mkdir(parents=True, exist_ok=True)
+        elif item == 'README.md':
+            archive.parent.mkdir(parents=True, exist_ok=True)
+            archive.write_text(f'##{name}\n\n{description}')
         else:
             archive.parent.mkdir(parents=True, exist_ok=True)
             archive.touch()
@@ -89,6 +104,14 @@ def select_template(template):
     global selected_template
     selected_template = template
     create_button.configure(state='normal', text=f'Create {selected_template} repo', fg_color='green', command=create_repo)
+
+def choose_folder():
+    folder = filedialog.askdirectory()
+    if folder:
+        global selected_folder
+        selected_folder = folder
+        global folder_label
+        folder_label.configure(text=folder)
 
 def start():
     
@@ -119,7 +142,14 @@ def start():
 
     for i in templates:
        tec = ctk.CTkButton(RepoForge, text=i, command=lambda t=i: select_template(t))
-       tec.pack(pady=5) 
+       tec.pack(pady=5)
+    
+    global folder_label
+    folder_label = ctk.CTkLabel(RepoForge, text='No folder selected')
+    folder_label.pack()        
+    
+    folder_button = ctk.CTkButton(RepoForge, text='Choose folder', command=choose_folder)
+    folder_button.pack(pady=5)
 
     global create_button
     create_button = ctk.CTkButton(RepoForge, text='Select a tecnology first', state='disabled')
@@ -129,9 +159,7 @@ def start():
     info_label = ctk.CTkLabel(RepoForge, text='', font=font1)
     info_label.pack(pady=5)
 
-title = ctk.CTkLabel(RepoForge, text='RepoForge', font=font2)
 
-title.pack(pady=10)
 new_project = ctk.CTkButton(RepoForge, text='New Project[+]', command=start)
 new_project.pack(pady=10)
 
